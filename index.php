327fbbc66cfdddf856ce8d401ecd014b7e65660e
admin <michal.zapart1984@gmail.com>
2026-01-28 18:54:44 +0100

    Poprawiamy stronke

--- index.php
+++ index.php
@@ -15,6 +15,7 @@ ini_set('display_startup_errors', 1);
         <?php
             include ("include/hero.html");
             include ("include/donejty_tabela.html");
+            include ("include/disclaimer.html");
         ?>
 
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
